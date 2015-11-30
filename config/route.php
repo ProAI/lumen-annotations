f98410245f16643faad3ada701875c5653e2b01c
Koga1988 <markuswetzel@gmx.net>
2015-11-30 16:49:49 +0100

    Add auto scan

--- config/route.php
+++ config/route.php
@@ -11,7 +11,19 @@ return [
     | `php artisan route:create` command.
     |
     */
-
+    
     'controllers_namespace' => 'App\Http\Controllers',
+
+    /*
+    |--------------------------------------------------------------------------
+    | Auto Scan
+    |--------------------------------------------------------------------------
+    |
+    | Automatically scan entity classes and update database on page load. This
+    | Option is useful in development mode.
+    |
+    */
+    
+    'auto_scan' => env('APP_AUTO_SCAN', false),
 
 ];
--- src/RouteAnnotationsServiceProvider.php
+++ src/RouteAnnotationsServiceProvider.php
@@ -16,6 +16,9 @@ class RouteAnnotationsServiceProvider extends ServiceProvider
         $this->registerConfig();
 
         $this->app->register('ProAI\RouteAnnotations\Providers\CommandsServiceProvider');
+
+        if ($this->app->config('route.annotations.auto_scan'))
+            $this->autoUpdateDatabase();
     }
 
     /**
@@ -26,5 +29,24 @@ class RouteAnnotationsServiceProvider extends ServiceProvider
     protected function registerConfig()
     {
         $this->app->configure('route');
+    }
+
+    /**
+     * Scan route annotations and update routes.
+     *
+     * @return void
+     */
+    public function updateRoutes()
+    {
+        $app = app();
+
+        // get classes
+        $classes = $app['route.annotations.classfinder']->getClassesFromNamespace(config('route.controllers_namespace'));
+
+        // build metadata
+        $routes = $app['route.annotations.scanner']->scan($classes);
+
+        // generate routes.php file for scanned routes
+        $app['route.annotations.generator']->generate($routes);
     }
 }
